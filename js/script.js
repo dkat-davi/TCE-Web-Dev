1533fb328ad01a35d1257944d9c68e9bae4f1210
DKAT-DAVI <eudkatdavikalel@gmail.com>
2023-04-28 23:54:52 -0300

    Criando o header padrão do site

--- js/script.js
+++ js/script.js
@@ -3,13 +3,29 @@ const handleDropdonwMenuClick = () => {
     const dropdownMenuIcon = document.querySelector('#menu-icon')
 
     if(options.style.display === 'none') {
-        options.style.display = 'block' 
+        options.style.display = 'flex' 
         dropdownMenuIcon.classList.remove('fa-bars')
         dropdownMenuIcon.classList.add('fa-x')
     }
-    else {
+    else {        
         options.style.display = 'none' 
         dropdownMenuIcon.classList.add('fa-bars')
         dropdownMenuIcon.classList.remove('fa-x')
+    }
+}
+
+const handleDropdonwMenuReferencesClick = () => {
+    const options = document.querySelector('#references')
+    const dropdownMenuIcon = document.querySelector('#references-menu-icon')
+
+    if(options.style.display === 'none') {
+        options.style.display = 'flex' 
+        dropdownMenuIcon.classList.remove('fa-angle-down')
+        dropdownMenuIcon.classList.add('fa-angle-up')
+    }
+    else {
+        options.style.display = 'none' 
+        dropdownMenuIcon.classList.remove('fa-angle-up')
+        dropdownMenuIcon.classList.add('fa-angle-down')
     }
 }
