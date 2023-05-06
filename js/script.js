9c2aa17bf4dd402bb93219e7a683b51f210a1fa5
DKAT-DAVI <eudkatdavikalel@gmail.com>
2023-05-06 19:15:46 -0300

    Criando a página padrão de tags e trabalhando na mudança de página de tags

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+const windowWidth = this.window.innerWidth
+
 const handleDropdonwMenuClick = () => {
     const options = document.querySelector('nav')
     const dropdownMenuIcon = document.querySelector('#menu-icon')
@@ -48,10 +50,35 @@ const handleScrollLeft = () => {
       });
 }
 
+
+if (windowWidth <= '768') {
+    const screen = document.querySelector('html')
+    let initialPosition = null
+    
+    screen.addEventListener('touchstart', (event) => {
+        initialPosition = event.touches[0].clientX
+    })
+
+    screen.addEventListener('touchmove', (event) => {
+        if (initialPosition === null) {
+            return;
+        }
+
+        const currentPosition = event.changedTouches[0].clientX
+
+        if (currentPosition < initialPosition) {
+            window.location.assign('./button.html')
+        } else if ( currentPosition > initialPosition) {
+            window.location.assign('./h1.html')
+        }
+
+        initialPosition = null
+    })
+}
+
 window.addEventListener('resize', function() {
     const menu = document.querySelector('nav')
     const menuIcon = document.querySelector('#menu-icon')
-    const windowWidth = this.window.innerWidth
 
     menu.style.display = windowWidth <= '768' ? 'none' : 'block'
     menuIcon.classList.add('fa-bars')
